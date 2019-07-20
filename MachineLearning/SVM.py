515ec4761d98e0a78f2d10c2ff24a9a8f5ae6b9f
PC_zha <30690856+zzzpc@users.noreply.github.com>
2019-07-20 17:25:52 +0800

    Create SVM.py

---
+++ MachineLearning/SVM.py
@@ -0,0 +1,170 @@
+import numpy as np
+import  pandas as pd
+import  matplotlib.pyplot as plt
+from sklearn.datasets import  load_iris
+from sklearn.model_selection import  train_test_split
+from collections import  Counter
+from sklearn.neighbors import  KNeighborsClassifier
+
+
+def create_data():
+    iris = load_iris()
+    df = pd.DataFrame(iris.data, columns=iris.feature_names)
+    df['label'] = iris.target
+    df.columns = [
+        'sepal length', 'sepal width', 'petal length', 'petal width', 'label'
+    ]
+    data = np.array(df.iloc[:100, [0, 1, -1]])
+    for i in range(len(data)):
+        if data[i, -1] == 0:
+            data[i, -1] = -1
+    # print(data)
+    return data[:, :2], data[:, -1]
+
+
+X, y = create_data()
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
+
+class SVM :
+    def __init__(self,max_iter=100,kernel='linear'):
+        self.max_iter=max_iter
+        self._kernel=kernel
+
+    def init_args(self,feature,label):#参数初始化
+        self.m,self.n=feature.shape
+        self.X=feature
+        self.y=label
+        self.b=0.0
+
+        self.alpha=np.ones(self.m)
+        self.E=[self._E[i] for i in range(self.m)]
+
+        self.C=1.0#松弛变量
+
+    def _KKT(self,i):
+         y_g=self._g(i)*self.y[i]   #KKT条件
+         if self.alpha[i] ==0 :
+             return y_g >=1
+         elif 0<self.alpha[i]<self.C :
+             return  y_g == 1
+         else :
+             return y_g<=1
+
+    # g(x)预测值，输入xi（X[i]）  分类决策函数(sign  1-n的求和)
+    def _g(self,i):
+        r=self.b
+        for j in range(self.m) :
+            r+=self.alpha[j] *self.Y[j] *self._kernel(self.X[i],self.X[j])
+        return r
+
+    #核函数
+    def kernel(self,x1,x2):
+        if self._kernel =='linear' :
+            return sum([x1[k]*x2[k] for k in range(self.n)])
+        elif self.kernel == 'poly':
+            return (sum([x1[k]*x2[k] for k in range(self.n)])+1)**2
+        return  0
+
+    # E（x）为g(x)对输入x的预测值和y的差
+    def _E(self,i):
+        return self.g(i)-self.y[i]
+
+    def _init_alpha(self):
+        # 外层循环首先遍历所有满足0<a<C的样本点，检验是否满足KKT
+        index_list=[i for i in range(self.m) if 0<self.alpha[i]<self.C]
+        #否则遍历整个训练集
+        non_satisfy_list=[i for i in range(self.m) if i not in index_list ]
+        index_list.extend(non_satisfy_list)
+        #append 与extend 的区别是  append是将整个添加的内容以对象的形式存进list中，而extend是一序列的形式填充进序列
+        for i in index_list:
+            if self._KKT(i) :
+                continue
+            E1=self.E[i]
+            # 如果E2是+，选择最小的；如果E2是负的，选择最大的
+            if E1 >= 0:
+                j=min(range(self.m),key=lambda x:self.E[x])
+            else:
+                j = max(range(self.m), key=lambda x: self.E[x])
+            return i, j
+
+    def _compare(self,_alpha,L,H): #满足alpha大于0小于C
+        if _alpha >H :
+            return  H
+        elif _alpha <L :
+            return L
+        else:
+            return _alpha
+    def fit(self,feature,labels):  #smo算法
+        self.init_args(feature,labels)
+        for t in range(self.max_iter):
+            i1,i2 =self._init_alpha()  #返回初始化后的i，j
+
+            if self.y[i1] ==self.y[i2]:
+                L=max(0,self.alpha[i1]+self.alpha[i2]-self.C)
+                H=min(self.C,self.alpha[i1]+self.alpha[i2])
+            else:
+                L=max(0,self.alpha[i2]-self.alpha[i1])
+                H=min(self.C,self.C+self.alpha[i2]-self.alpha[i1])
+
+            E1=self.E[i1]
+            E2=self.E[i2]
+            #eta=K11+K22-2K12
+            eta=self.kernel(self.X[i1],self.X[i2])+self.kernel(
+                self.X[i2],
+                self.X[i2])-2*self.kernel(self.X[i1],self.X[i2])
+            if eta <=0 :
+                continue
+
+
+            alpha2_new_unc=self.alpha[i2] +self.y[i2]*(
+                E1-E2)/eta
+            alpha2_new=self._compare(alpha2_new,L,H)
+            alpha1_new=self.alpha[i1]+self.y[i1]*self.y[i2]*(self.alpha[i2]-alpha2_new)
+
+            b1_new = -E1 - self.y[i1] * self.kernel(self.X[i1], self.X[i1]) * (
+                    alpha1_new - self.alpha[i1]) - self.y[i2] * self.kernel(
+                self.X[i2],
+                self.X[i1]) * (alpha2_new - self.alpha[i2]) + self.b
+            b2_new = -E2 - self.y[i1] * self.kernel(self.X[i1], self.X[i2]) * (
+                    alpha1_new - self.alpha[i1]) - self.y[i2] * self.kernel(
+                self.X[i2],
+                self.X[i2]) * (alpha2_new - self.alpha[i2]) + self.b
+
+            if 0 < alpha1_new < self.C:
+                b_new = b1_new
+            elif 0 < alpha2_new < self.C:
+                b_new = b2_new
+            else:
+                # 选择中点
+                b_new = (b1_new + b2_new) / 2
+
+                # 更新参数
+            self.alpha[i1] = alpha1_new
+            self.alpha[i2] = alpha2_new
+            self.b = b_new
+
+            self.E[i1] = self._E(i1)
+            self.E[i2] = self._E(i2)
+
+    def predict(self, data):
+            r = self.b
+            for i in range(self.m):
+                r += self.alpha[i] * self.y[i] * self.kernel(data, self.X[i])
+
+            return 1 if r > 0 else -1
+
+    def score(self, X_test, y_test):
+            right_count = 0
+            for i in range(len(X_test)):
+                result = self.predict(X_test[i])
+                if result == y_test[i]:
+                    right_count += 1
+            return right_count / len(X_test)
+
+    def _weight(self):
+            # linear model
+            yx = self.y.reshape(-1, 1) * self.X
+            self.w = np.dot(yx.T, self.alpha)
+            return self.w
+
+
